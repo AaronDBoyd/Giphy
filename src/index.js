5c6b1131f44489e48e30715ca10bb3ee85b2dc2f
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-21 19:32:00 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// // import './../css/styles.css';
+import './css/styles.css';
 import GiphyService from './js/giphy-service.js';
 
 function clearFields() {
   $('#searchKey').val("");
-  $('.showImages').load("");
+  $('.showImages').text("");
 }
+
+// function getElements(response) {
+//   for (let i = 0; i < response.data.length; i++) {
+//   if (response.data[i].images.downsized.url) {
+//     $('.showImages').append(`<img src="${response.data[i].images.downsized.url}">`);
+//   } else {
+//     $('.showErrors').text(`There was an error processing your request: ${response.message}`);
+//   }
+// }}
+
+// $(document).ready(function() {
+//   $('#enterSearch').click(function() {
+//     const keyword = $('#searchKey').val();
+//     clearFields();
+//     GiphyService.getGiphy(keyword)
+//     .then(function(response) {
+//       getElements(response);
+//     });
+//   });
+// });
+
+
+
 
 $(document).ready(function() {
   $('#enterSearch').click(function() {
-    const gif = $('#Giphy').val();
+    const keyword = $('#searchKey').val();
     clearFields();
     let promise = GiphyService.getGiphy(keyword);
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $('.showImages').load(`${body.data[0].images.original.url}`);
+      
+      $('.showImages').append(`<img src="${body.data[0].images.downsized.url}">`);
       //.text or ????
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
--- src/js/giphy-service.js
+++ src/js/giphy-service.js
@@ -1,21 +1,31 @@
 //Business Logic
 export default class GiphyService {
   static getGiphy(keyword) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `http://api.giphy.com/v1/gifs/search?q=${keyword}api_key=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    })
+    // return fetch(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${keyword}&limit=3`)
+    // .then(function(response) {
+    //   if (!response.ok) {
+    //     throw Error(response.statusText);
+    //   }
+    //   return response.json();
+    // })
+    // .catch(function(error) {
+    //   return error;
+    // })
+//   }
+// }
+
+
+return new Promise(function(resolve, reject) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${keyword}&limit=3&lang=en`;
+  request.onload = function() {
+    if (this.status === 200) {
+      resolve(request.response);
+    } else {
+      reject(request.response);
+    }
   }
-}
-
-
+  request.open("GET", url, true);
+  request.send();
+},
   
